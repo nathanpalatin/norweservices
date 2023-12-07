44b8f7b3ac5ced6c132f321e4bbecd28777a2b6c
Nathan Palatin <nath.palatin@gmail.com>
2023-12-07 13:03:51 -0300

    change icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Services } from '@/components/Services'
 
-import { FaSignal, FaHome, FaClipboardList, FaClipboardCheck, FaInfinity, FaRegCreditCard, FaCar, FaKey, FaCheckCircle, FaTree, FaBuilding, FaDollarSign } from "react-icons/fa"
+import { FaSignal, FaHome, FaClipboardList, FaClipboardCheck, FaInfinity, FaRegCreditCard, FaCar, FaKey, FaCheckCircle, FaTree, FaBuilding, FaDollarSign, FaEnvira } from "react-icons/fa"
 
 export default function Home() {
   return (
@@ -20,7 +20,7 @@ export default function Home() {
           <Services link="https://norwe.com.br/" title="Financiamento Veicular" icon={FaCar}   />
           <Services link="https://norwe.com.br/" title="Financiamento Imobiliário" icon={FaKey} />
           <Services link="https://norwe.com.br/" title="Consignado" icon={FaCheckCircle}  />
-          <Services link="https://norwe.com.br/" title="Serviços ao Agronegócio" icon={FaTree}  />
+          <Services link="https://norwe.com.br/" title="Serviços ao Agronegócio" icon={FaEnvira}  />
           <Services link="https://norwe.com.br/" title="Home Equity" icon={FaBuilding}  />
           <Services link="https://norwe.com.br/" title="Investimento Imobiliário" icon={FaHome}  />
           <Services link="https://norwe.com.br/" title="Câmbio" icon={FaDollarSign} />
