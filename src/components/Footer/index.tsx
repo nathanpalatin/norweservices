4c227756569130b7ec01fad6384fd548fcafae9a
Nathan Palatin <nath.palatin@gmail.com>
2023-12-07 13:08:47 -0300

    change image credit

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export function Footer() {
    return (
       <div className="relative flex justify-center flex-col items-center gap-3 mb-10 mt-6">
-         <Image className="rounded-lg" src={'/mockup_creditcardnw.png'} quality={100} alt="Mockup credit Norwe" width={340} height={100} />
+         <Image className="rounded-lg" src={'/mockup_creditcardok.png'} quality={100} alt="Mockup credit Norwe" width={340} height={100} />
          <h1 className="text-zinc-500 font-medium -mt-6 left-5 absolute italic">Em breve</h1>
          <Link href={'https://norwe.com.br'} className="text-title-service font-medium mt-2">Solicitar meu cartão físico</Link>
 
